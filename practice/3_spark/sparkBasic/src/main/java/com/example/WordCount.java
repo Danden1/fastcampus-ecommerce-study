4c402a07fc603c2fecce2bec6bbb0e962ed8a7ef
Yongha Kwon <yongha.kwon12@gmail.com>
2025-10-27 00:39:52 +0900

    feat: spark 기초 실습 코드 추가

--- practice/3_spark/sparkBasic/src/main/java/com/example/WordCount.java
+++ practice/3_spark/sparkBasic/src/main/java/com/example/WordCount.java
@@ -1,8 +1,33 @@
 package com.example;
 
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+
 public class WordCount {
     public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("Word Count")
+                .master("local[*]")
+                .getOrCreate();
 
+
+        Dataset<String> lines = spark.read().textFile("test.txt");
+
+        Dataset<String> words = lines.flatMap(
+                (FlatMapFunction<String, String>)  line -> Arrays.asList(line.split(" ")).iterator(),
+                Encoders.STRING()
+        );
+
+        Dataset<Row> wordCounts = words.groupBy("value")
+                .count()
+                .withColumnRenamed("count", "wordCount");
+
+        wordCounts.show();
 
     }
 }
